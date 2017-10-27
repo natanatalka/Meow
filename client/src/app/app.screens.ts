0ac5c6f4f118a2add6a5f4d921b078bf0e9a6ce7
test <test@test.com>
2017-10-27 16:55:38 +0300

    [Studio] Delete screen "Ssss"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,21 +2,16 @@
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { Blank_PhonePortrait } from '../pages/Blank/PhonePortrait/Blank';
-import { Ssss_PhonePortrait } from '../pages/Ssss/PhonePortrait/Ssss';
 import { 111 _PhonePortrait } from '../pages/111/PhonePortrait/111';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
     Blank_PhonePortrait,
-    Ssss_PhonePortrait,
     111 _PhonePortrait
   ];
   static mapping = {
     'Blank': {
       PhonePortrait: Blank_PhonePortrait
-    },
-    'Ssss': {
-      PhonePortrait: Ssss_PhonePortrait
     },
     '111': {
       PhonePortrait: 111 _PhonePortrait
--- client/src/pages/Ssss/PhonePortrait/Ssss.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-ssss-phoneportrait',
-  templateUrl: 'Ssss.html'
-})
-export class Ssss_PhonePortrait extends Screen {
-}
