13f4e3e7bb2a2a5c985f1d4799cb2a4f5805cc85
test <test@test.com>
2017-10-27 16:54:37 +0300

    [Studio] Add screen "ssss"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,14 +2,19 @@
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { Blank_PhonePortrait } from '../pages/Blank/PhonePortrait/Blank';
+import { Ssss_PhonePortrait } from '../pages/Ssss/PhonePortrait/Ssss';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
-    Blank_PhonePortrait
+    Blank_PhonePortrait,
+    Ssss_PhonePortrait
   ];
   static mapping = {
     'Blank': {
       PhonePortrait: Blank_PhonePortrait
+    },
+    'Ssss': {
+      PhonePortrait: Ssss_PhonePortrait
     }
   }
 }
---
+++ client/src/pages/Ssss/PhonePortrait/Ssss.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-ssss-phoneportrait',
+  templateUrl: 'Ssss.html'
+})
+export class Ssss_PhonePortrait extends Screen {
+}
